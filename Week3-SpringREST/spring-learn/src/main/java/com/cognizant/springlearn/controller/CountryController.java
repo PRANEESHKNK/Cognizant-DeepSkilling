41eda891fcfa556587d756ed31e315243dd1e680
Praneesh <kolachanapraneesh@gmail.com>
2026-06-30 23:55:21 +0530

    Week 3: Completed Spring REST Hands-on 1, 2 and 3

--- Week3-SpringREST/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ Week3-SpringREST/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -3,6 +3,7 @@ package com.cognizant.springlearn.controller;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognizant.springlearn.model.Country;
@@ -16,8 +17,15 @@ public class CountryController {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("country.xml");
 
-        Country country = context.getBean("country", Country.class);
+        return context.getBean("IN", Country.class);
+    }
 
-        return country;
+    @GetMapping("/countries/{code}")
+    public Country getCountryByCode(@PathVariable String code) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        return context.getBean(code, Country.class);
     }
 }
